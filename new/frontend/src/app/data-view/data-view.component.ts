597939fe9028cc9735620f91706364df1391134c
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-04-16 23:29:38 +0530

    add error handling

--- new/frontend/src/app/data-view/data-view.component.ts
+++ new/frontend/src/app/data-view/data-view.component.ts
@@ -2,26 +2,36 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Person} from "../models/person";
 import {DataService} from "../services/data.service";
 import {SpinnerComponent} from "../spinner/spinner.component";
+import { ErrorBannerComponent } from "../error-banner/error-banner.component";
 
 @Component({
     selector: 'app-data-view',
     imports: [
-        SpinnerComponent
-    ],
+    SpinnerComponent,
+    ErrorBannerComponent
+],
     templateUrl: './data-view.component.html'
 })
 export class DataViewComponent implements OnInit {
     @Input() urlId: string = 'home';
     data: Person[] = [];
     isLoading: boolean = true;
+    errorMessage!: string;
 
     constructor(private dataService: DataService) {
     }
 
     ngOnInit() {
-        this.dataService.callBackendApi(this.urlId).subscribe(data => {
-            this.data = data;
-            this.isLoading = false;
-        });
+        this.dataService.callBackendApi(this.urlId).subscribe({
+            next: (data) => {
+                this.data = data
+                this.isLoading = false
+            },
+            error: (e) => {
+                console.error(e)
+                this.errorMessage = e.message
+                this.isLoading = false
+            }
+        })
     }
 }
---
+++ new/frontend/src/app/error-banner/error-banner.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-error-banner',
+    imports: [],
+    templateUrl: './error-banner.component.html'
+})
+export class ErrorBannerComponent {
+    @Input() errorMessage: string = '';
+}
