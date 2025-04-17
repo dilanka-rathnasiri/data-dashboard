b5d2d6cc64f667597d383a72a34ecef2f525fd09
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-04-17 23:21:48 +0530

    implement basic filtering

--- new/frontend/src/app/data-view/data-view.component.ts
+++ new/frontend/src/app/data-view/data-view.component.ts
@@ -3,20 +3,32 @@ import {Person} from "../models/person";
 import {DataService} from "../services/data.service";
 import {SpinnerComponent} from "../spinner/spinner.component";
 import { ErrorBannerComponent } from "../error-banner/error-banner.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-data-view',
     imports: [
     SpinnerComponent,
-    ErrorBannerComponent
+    ErrorBannerComponent,
+    FormsModule
 ],
     templateUrl: './data-view.component.html'
 })
 export class DataViewComponent implements OnInit {
     @Input() urlId: string = 'home';
     data: Person[] = [];
+    filteredData: Person[] = [];
     isLoading: boolean = true;
     errorMessage!: string;
+
+    filters = {
+        n: '',
+        id: '',
+        name: '',
+        address: '',
+        birthDate: '',
+        email: ''
+    };
 
     constructor(private dataService: DataService) {
     }
@@ -24,14 +36,25 @@ export class DataViewComponent implements OnInit {
     ngOnInit() {
         this.dataService.callBackendApi(this.urlId).subscribe({
             next: (data) => {
-                this.data = data
-                this.isLoading = false
+                this.data = data;
+                this.filteredData = data;
+                this.isLoading = false;
             },
             error: (e) => {
-                console.error(e)
-                this.errorMessage = e.message
-                this.isLoading = false
+                console.error(e);
+                this.errorMessage = e.message;
+                this.isLoading = false;
             }
-        })
+        });
+    }
+
+    applyFilters() {
+        this.filteredData = this.data.filter(person => {
+            return Object.keys(this.filters).every(key => {
+                const filterValue = this.filters[key as keyof Person].toLowerCase();
+                const personValue = (person[key as keyof Person] || '').toString().toLowerCase();
+                return personValue.includes(filterValue);
+            });
+        });
     }
 }
